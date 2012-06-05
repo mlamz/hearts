a029253ff2563ea769ac19f11b16dbbd42d04cfb
Michael Lam <michael@michaellam.co.uk>
2012-06-05 20:30:35 +0100

    added card ranks

--- public/js/models/card-properties.js
+++ public/js/models/card-properties.js
@@ -6,6 +6,21 @@ define([],
                 diamonds: 1,
                 spades: 2,
                 hearts: 3
+            },
+            rank: {
+                two: 2,
+                three: 3,
+                four: 4,
+                five: 5,
+                six: 6,
+                seven: 7,
+                eight: 8,
+                nine: 9,
+                ten: 10,
+                jack: 11,
+                queen: 12,
+                king: 13,
+                ace: 14
             }
         }
 	}
--- public/js/tests/spec/models/card-properties-spec.js
+++ public/js/tests/spec/models/card-properties-spec.js
@@ -8,6 +8,22 @@ define(['../../../models/card-properties'],
 			expect(CardProperties.suit.spades).toEqual(2);
 			expect(CardProperties.suit.hearts).toEqual(3);
 		});
+
+		it("should have all card ranks", function() {
+			expect(CardProperties.rank.two).toEqual(2);
+			expect(CardProperties.rank.three).toEqual(3);
+			expect(CardProperties.rank.four).toEqual(4);
+			expect(CardProperties.rank.five).toEqual(5);
+			expect(CardProperties.rank.six).toEqual(6);
+			expect(CardProperties.rank.seven).toEqual(7);
+			expect(CardProperties.rank.eight).toEqual(8);
+			expect(CardProperties.rank.nine).toEqual(9);
+			expect(CardProperties.rank.ten).toEqual(10);
+			expect(CardProperties.rank.jack).toEqual(11);
+			expect(CardProperties.rank.queen).toEqual(12);
+			expect(CardProperties.rank.king).toEqual(13);
+			expect(CardProperties.rank.ace).toEqual(14);
+		})
       });
   }
 )
