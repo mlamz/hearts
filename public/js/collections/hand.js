04aa88c618111d76629f1c6d32e5c7e7ec385451
Michael Lam <treadsafely@gmail.com>
2012-06-06 17:15:08 +0100

    hands now contain randomly assigned cards from a deck

--- public/js/collections/hand.js
+++ public/js/collections/hand.js
@@ -1,17 +1,31 @@
 define(['backbone', '../models/card'], 
-	function(Backbone, Card) {
+	function(Backbone,  Card) {
     	var Hand = Backbone.Collection.extend({
     		model: Card,
-    		initialize: function(){
-    			this.populateHand();
+    		initialize: function(deck){
+                this.models = [];
+    			this.populateHand(deck);
     		},
-    		populateHand: function(){
+    		populateHand: function(deck){
     			var i;
-    			for (i = 0;i<13;i++){
-    				this.add(new Card());
-    			}
+                for(i = 0; i < 13; i++){
+                    var card = takeCardFromDeck(deck);
+                    this.add(card);
+                }
     		}
     	});
+
+        function takeCardFromDeck(deck){
+            var randonIndex
+            ,   randomCard;
+
+            randomIndex = Math.floor(Math.random() * (deck.length));
+
+            randomCard = deck.models[randomIndex];
+            deck.remove(randomCard);
+            return randomCard;
+        }
+
     	return Hand;
     }
 );
--- public/js/tests/spec/collections/hand-spec.js
+++ public/js/tests/spec/collections/hand-spec.js
@@ -1,32 +1,31 @@
 define([	
-	'../../../models/card-properties'
+	'underscore'
+,	'../../../models/card-properties'
 ,	'../../../models/card'
 ,	'../../../collections/hand'
+,	'../../../collections/deck'
 ], 
-  function(CardProperties, Card, Hand) {
+  function(_, CardProperties, Card, Hand, Deck) {
+
+
     describe("Hand", function() {
-
-		it("can add cards to a hand", function() {
-			var hand = new Hand();
-
-			var cardsInHand = hand.length;
-			var card = new Card({ rank: CardProperties.rank.ace, suit: CardProperties.suit.hearts });
-			
-			hand.add(card);
-			expect(hand.length).toEqual(cardsInHand + 1);
-		});
-
 		it("should be initialised with 13 cards", function() {
-			var hand = new Hand();
+			var hand = new Hand(new Deck());
 
-			expect(hand.length).toEqual(13);
+			expect(hand.models.length).toEqual(13);
+
+			for(var i = 0;i<13;i++){
+				expect(hand.models[i].get('suit')).toBeDefined();
+				expect(hand.models[i].get('rank')).toBeDefined();
+			}
 		});
 
 		it("initialising four hands should randomly assign a pack of cards between the hands", function(){
-			var hand1 = new Hand()
-			,	hand2 = new Hand()
-			,	hand3 = new Hand()
-			,	hand4 = new Hand()
+			var deck = new Deck()
+			,	hand1 = new Hand(deck)
+			,	hand2 = new Hand(deck)
+			,	hand3 = new Hand(deck)
+			,	hand4 = new Hand(deck)
 			,	myPush = Array.prototype.push;
 
 			var models = [];
@@ -37,8 +36,11 @@ define([
 
 			var cards = getCardConfigurations();
 
-			for(var card in cards){
-				//expect(models.indexOf(card)).toBeGreaterThan(0);
+			for(var i = 0;i<52;i++){
+				var model = _.find(models, function(model){ 
+					return model.get('suit') == cards[i].get('suit') && model.get('rank') == cards[i].get('rank');
+				});
+				expect(model).toBeDefined();
 			}
 
 
@@ -49,9 +51,9 @@ define([
     	var cards = [];
     	for(var suit in CardProperties.suit){
     		for(var rank in CardProperties.rank){
-    			cards.push(new Card({suit:suit,rank:rank}));
+    			cards.push(new Card({suit:CardProperties.suit[suit],rank:CardProperties.rank[rank]}));
     		}
-    	}
+		}
 
     	return cards;
     }
