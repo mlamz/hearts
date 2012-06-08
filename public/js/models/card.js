1eec1818a3fbf6d38a1bd40c8043efc60ca8a686
Michael Lam <michael@michaellam.co.uk>
2012-06-08 09:08:37 +0100

    card points values and tests

--- public/js/models/card.js
+++ public/js/models/card.js
@@ -1,9 +1,10 @@
-define(['backbone'], 
-	function(Backbone) {
+define(['backbone','cardProperties'], 
+	function(Backbone, CardProperties) {
     	var Card = Backbone.Model.extend({
     		initialize: function(){
                 this.rank = this.get('rank');
                 this.suit = this.get('suit');
+                this.points = getPoints(this);
     		}
     	});
     	Card.prototype.equals = function(otherCard){
@@ -13,6 +14,19 @@ define(['backbone'],
 
     		return false;
     	};
+
+        function getPoints(card){
+            if (card.suit === CardProperties.suit.spades && card.rank === CardProperties.rank.queen){
+                return 13;
+            }
+
+            if(card.suit === CardProperties.suit.hearts){
+                return 1;
+            }
+
+            return 0;
+        }
+
     	return Card;
 	}
 );
--- public/js/tests/spec/models/card-spec.js
+++ public/js/tests/spec/models/card-spec.js
@@ -1,5 +1,5 @@
-define(['cardProperties','card'], 
-  function(CardProperties, Card) {
+define(['underscore','cardProperties','card','deck'], 
+  function(_, CardProperties, Card, Deck) {
     describe("Card", function() {
 
 		it("can be initialised with a rank and a suit", function() {
@@ -26,6 +26,37 @@ define(['cardProperties','card'],
 
 			expect(card.equals(card2)).toEqual(false);
 			expect(card3.equals(card4)).toEqual(false);
+		});
+
+		it("the queen of spades should be 13 points", function(){
+			var card = new Card({ rank: CardProperties.rank.queen, suit: CardProperties.suit.spades });
+
+			expect(card.points).toEqual(13);
+		});
+
+		it("all hearts should be 1 point", function(){
+			var deck = new Deck();
+
+			hearts = _.filter(deck.models, function(card){ return card.suit === CardProperties.suit.hearts});
+
+			_.each(hearts, function(card){
+				expect(card.points).toEqual(1);
+			});
+			
+		});
+
+		it("all other cards should be 0 points", function(){
+			var deck = new Deck()
+			, 	queenOfSpades
+			,	zeroPointCards;
+			
+			queenOfSpades = new Card({ rank: CardProperties.rank.queen, suit: CardProperties.suit.spades });
+
+			zeroPointCards = _.filter(deck.models, function(card){ return !card.equals(queenOfSpades) && card.suit !== CardProperties.suit.hearts});
+			
+			_.each(zeroPointCards, function(card){ 
+				expect(card.points).toEqual(0);
+			})
 		});
       });
   }
