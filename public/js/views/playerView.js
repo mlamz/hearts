99edaa71213739f8ff46cba57a2b6adc53efc6fd
Michael Lam <treadsafely@gmail.com>
2012-06-25 14:10:56 +0100

    add and display card images

--- public/js/views/playerView.js
+++ public/js/views/playerView.js
@@ -2,12 +2,14 @@ define(['jquery', 'underscore', 'backbone','player'],
 	function($, _, Backbone, Player) {
 		
 		var PlayerView = Backbone.View.extend({
+			html: $("#template-player").html(),
 			template: _.template($("#template-player").html()),
 			initialize: function(player){
 				this.player = player;
 				console.log(player.name + " initialized");
 				console.log(player.hand.models);
 				this.el = $('#player-' + this.player.number);
+				//this.el.find('img').addAttr('src', "httpp.....")
 				this.render();
 			
 			},
