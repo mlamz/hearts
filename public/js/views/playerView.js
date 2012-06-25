184fcf22130a2e53f5b8f88056e4e79be37440a3
Michael Lam <treadsafely@gmail.com>
2012-06-25 11:49:19 +0100

    add player template

--- public/js/views/playerView.js
+++ public/js/views/playerView.js
@@ -2,10 +2,17 @@ define(['jquery', 'underscore', 'backbone','player'],
 	function($, _, Backbone, Player) {
 		
 		var PlayerView = Backbone.View.extend({
-
+			template: _.template($("#template-player").html()),
 			initialize: function(player){
 				this.player = player;
 				console.log(player.name + " initialized");
+				this.el = $('#player-' + this.player.number);
+				this.render();
+			
+			},
+			render: function(){
+				var html = this.template({ name: this.player.name });
+    			$(this.el).append(html);
 			}
 		});
 
